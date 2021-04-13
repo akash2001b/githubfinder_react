4ce058f5be3ba058b1702383034f4ae9b9487577
Gunner70 <samfrenkinson@gmail.com>
2021-04-13 23:00:37 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  foo(){
+    return 'akash';
+  }
+
+  render(){
+
+    const loading=false;
+    const name='akash';    
+    return (
+      <div className="App">
+        <h2>My app</h2>
+        { loading ? <h1>Hello from {this.foo()}</h1> : <h4> Loading</h4> }
+      </div>
+    );
+
+  }
 }
 
 export default App;
